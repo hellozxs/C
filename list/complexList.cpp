37f4511c6c9e73b22552ac24f57036afeb6be06a
niuli <526591420@qq.com>
2016-08-20 23:55:21 +0800

    复杂链表的复制：更优的解法

--- list/complexList.cpp
+++ list/complexList.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-const hashTableSize = 100;
+const int hashTableSize = 100;
 typedef struct complexListNode
 {
     int              _data;
@@ -27,43 +27,126 @@ typedef struct hashNode
 }hashNode;
 
 
+////////////////////////    利用哈希表实现   ////////////////////////////////////
+//listNode* Clone(listNode* pHead)
+//{
+//    if(pHead == NULL)
+//        return NULL;
+//    hashTable<int,int> ht(hashTableSize);
+//    listNode* pHead1 = new listNode(pHead1->_data);
+//    ht.Insert(pHead, pHead1);
+//
+//    listNode* cur  = pHead->_next;
+//    listNode* cur1 = pHead1;
+//    while(cur)
+//    {
+//        listNode* tmp = new listNode(cur->_data);
+//        cur = cur->_next;
+//        cur1->_next = tmp;
+//        cur1 = cur1->_next;
+//        ht.Insert(cur, tmp);
+//    }
+//
+//    cur = pHead;
+//    cur1 = pHead1;
+//    while(cur)
+//    {
+//        if(cur->_sibling != NULL)
+//        {
+//            cur1->_sibling = ht.Find(cur1->_sibling)->_value;
+//        }
+//        cur1 = cur1->_next;
+//        cur  = cur->_next;
+//    }
+//
+//    return pHead1;
+//}
+//
 
-listNode* Clone(listNode* pHead)
+///////////////////  更优的解法  ////////////////////////////
+
+listNode* clone_Pro (listNode* pHead)
 {
     if(pHead == NULL)
-        return;
-    hashTable<int,int> ht(hashTableSize);
-    listNode* pHead1 = new listNode(pHead1->_data);
-    ht.Insert(pHead, pHead1);
+        return NULL;
 
-    listNode* cur  = pHead->_next;
-    listNode* cur1 = pHead1;
+    //第一步：插入复制结点
+    listNode* pHead1 = NULL;
+    listNode* cur = pHead;
     while(cur)
     {
-        listNode* tmp = new listNode(cur->_data);
-        cur = cur->_next;
-        cur1->_next = tmp;
-        cur1 = cur1->_next;
-        ht.Insert(cur, tmp);
+        listNode* cur1 = new listNode(cur->_data);
+        cur1->_next = cur->_next;
+        cur->_next = cur1;
+        cur = cur->_next->_next;
     }
 
+    //第二步：复制sibling
     cur = pHead;
-    cur1 = pHead1;
+    listNode* next = cur->_next;
     while(cur)
     {
-        if(cur->_sibling != NULL)
-        {
-            cur1->_sibling = ht.Find(cur1->_sibling)->_value;
-        }
-        cur1 = cur1->_next;
-        cur  = cur->_next;
+        next = cur->_next;
+        if(cur->_sibling)
+            next->_sibling = cur->_sibling->_next;
+        cur = cur->_next->_next;
     }
 
+    //第三步：分离链表
+    pHead1 = pHead->_next;
+    next = pHead1;
+    cur = pHead;
+    while(cur)
+    {
+        cur->_next = cur->_next->_next;
+        if(next->_next)
+            next->_next = next->_next;
+        cur = cur->_next;
+        next = next->_next;
+    }
     return pHead1;
 }
 
 
+void complexListPrint(listNode* pHead)
+{
+    listNode* cur = pHead;
+    while(cur)
+    {
+        std::cout<<"data:"<<cur->_data;
+        if(cur->_sibling )
+            std::cout<<"  sibling:"<<cur->_sibling->_data<<std::endl;
+        else
+            std::cout<<"  sibling:NULL"<<std::endl;
+        cur = cur->_next;
+    }
+}
+
+
+
 int main()
 {
+    listNode* pHead = new listNode(1);
+    listNode* cur1 = pHead;
+    cur1->_next = new listNode(2);
+    listNode* cur2 = cur1->_next;
+    cur2->_next = new listNode(3);
+    listNode* cur3 = cur2->_next;
+    cur3->_next = new listNode(4);
+    listNode* cur4 = cur3->_next;
+    cur4->_next = new listNode(5);
+    listNode* cur5 = cur4->_next;
+
+    cur1->_sibling = cur3;
+    cur2->_sibling = cur5;
+    cur4->_sibling = cur2;
+
+    complexListPrint(pHead);
+    complexListPrint(clone_Pro(pHead));
+    complexListPrint(pHead);
+    complexListPrint(clone_Pro(NULL));
+
+
+
     return 0;
 }
