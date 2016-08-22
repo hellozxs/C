844f415aa50ad079508a7579e3e7fb29e72b88a1
niuli <526591420@qq.com>
2016-08-22 13:26:59 +0800

    出栈顺序的合法性检测

---
+++ stack/isPopOrder.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+
+
+bool isPopOrder(int* arrPush, int* arrPop, size_t size)
+{
+    std::stack<int> s;
+    int popIndex = 0, pushIndex = 0;
+    for( ; popIndex < size; popIndex++ )
+    {
+        if(!s.empty() && s.top() == arrPop[popIndex])
+        {
+            s.pop();
+            continue;
+        }
+        while(pushIndex < size && arrPush[pushIndex] != arrPop[popIndex])
+            s.push(arrPush[pushIndex++]);
+        if(pushIndex >= size)
+            return false;
+        pushIndex++;
+    }
+    return true;
+}
+
+
+
+int main()
+{
+    int arrPush[] = {1, 2, 3, 4, 5};
+    //int arrPop[] = {3, 5, 4, 2, 1};
+    //int arrPop[] = {3, 5, 2, 4, 1};
+    int arrPop[] = {1, 2, 3, 4, 5};
+
+
+    std::cout<<isPopOrder(arrPush, arrPop, sizeof(arrPush)/sizeof(arrPush[0]))<<std::endl;
+
+    return 0;
+}
